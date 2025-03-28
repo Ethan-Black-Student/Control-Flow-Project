aa4e2b0214f9b92dc7d8e66fef36e96f0b41dbcb
Ethan Black [Student] <1blacketh2@hdsb.ca>
2025-03-28 14:31:34 -0400

    Update Controlflow.py

--- Controlflow.py
+++ Controlflow.py
@@ -6,7 +6,7 @@ OPEN_AI_KEY = "sk-proj-6vbRu_4d2Qw33aEayJjmst3PVGpCyUNsCX5rHkrJQ11AxX8Ah-Lw0EFFC
 
 print("Welcome")
 print("Let's ask ChatGPT something within our code:")
-question = str(input("heyyo "))
+question = str(input("You wake up in a cold dark school, what do you wanna do? "))
 
 client = OpenAI(api_key=OPEN_AI_KEY)
 
@@ -14,7 +14,7 @@ completion = client.chat.completions.create(
   model="gpt-4o-mini",
   store=True,
   messages=[
-    {"role": "system", "content": "You are a friendly Canadian. All your responses should be in Canadian slang"},
+    {"role": "system", "content": "You are a dnd like gamemaster, narrating what the outcomes of choices are in only a short sentance, be creative anything is on the table and make sure you leave room for a next action!"},
     {"role": "user", "content": question}
   ]
 )
