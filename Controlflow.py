de7d516041253f3aba307c549cc07fca63f6ae70
matthewrobinson17 <1robinsonmat@hdsb.ca>
2025-03-28 14:20:55 -0400

    Update Controlflow.py

--- Controlflow.py
+++ Controlflow.py
@@ -1,1 +1,21 @@
-print("hello world")
+#print("hello world")
+
+from openai import OpenAI
+
+OPEN_AI_KEY = "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"
+
+print("Welcome")
+print("Let's ask ChatGPT something within our code:")
+question = str(input("heyyo "))
+
+client = OpenAI(api_key=OPEN_AI_KEY)
+
+completion = client.chat.completions.create(
+  model="gpt-4o-mini",
+  store=True,
+  messages=[
+    {"role": "system", "content": "You are a friendly Canadian. All your responses should be in Canadian slang"},
+    {"role": "user", "content": question}
+  ]
+)
+print(completion.choices[0].message.content)
