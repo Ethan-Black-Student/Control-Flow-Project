ce407764581773b6f2c62779b234b23e6bcabf6e
matthewrobinson17 <1robinsonmat@hdsb.ca>
2025-04-03 13:46:41 -0400

    idk

--- Matthew.py
+++ Matthew.py
@@ -2,150 +2,45 @@ from openai import OpenAI
 
 import time
 
-"""
 
-OPEN_AI_KEY = "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"
-
-print("Welcome")
-print("Let's ask ChatGPT something within our code:")
-question = str(input("You wake up in a cold dark school, what do you wanna do? "))
-
-client = OpenAI(api_key=OPEN_AI_KEY)
-
-completion = client.chat.completions.create(
-  model="gpt-4o-mini",
-  store=True,
-  messages=[
-    {"role": "system", "content": "You are a dnd like gamemaster, narrating what the outcomes of choices are in only a short sentance, be creative anything is on the table and make sure you leave room for a next action!"},
-    {"role": "user", "content": question}
-  ]
-)
-
-response = (completion.choices[0].message.content)
-print(response)
-
-"""
-
+#Choice to fortify room
 print()
-print("Welcome to ze stori game")
-print()
-
-time.sleep (1.5)
-
-print("You wake up in an abandomed mental hospital, and your only goal is to survive the night!")
-
-time.sleep (1.5)
-
-print("Investigate soroundings (i) or go back to bed (b)")
-
-time.sleep (1.5)
-
-choice = input("What do you wanna do? ").strip().lower()
-
-while choice != "i":
-
-  if choice == "b":
-
-    print("Once again, you wake up in an abandomed mental hospital, and your only goal is to survive the night!")
-
-    time.sleep (1.5)
-
-    print("Investigate soroundings (i) or go back to bed (b)")
-
-    time.sleep (1.5)
-
-    choice = input("What do you wanna do? ").strip().lower()
-
-  else:
-
-    print("Seriosly now, you wake up in an abandomed mental hospital, and your only goal is to survive the night!")
-
-    time.sleep (1.5)
-
-    print("Investigate soroundings (i) or go back to bed (b)")
-
-    time.sleep (1.5)
-
-    choice = input("What do you wanna do? ").strip().lower()
-
-OPEN_AI_KEY = "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"
-
-print("You spot a phone resting on your side table, but as you start to reach for it, it starts to ring -- someone is calling! Now is your chance to find out whats really going on, but the phone is almost dead!!")
-question = str(input(""))
-
-client = OpenAI(api_key=OPEN_AI_KEY)
-
-completion = client.chat.completions.create(
-  model="gpt-4o-mini",
-  store=True,
-  messages=[
-    {"role": "system", "content": "you are calling somone inside a mental hospital, there has been an attack and a mysterious creature is inside, the hospital is now abandonded. but the person your calling does not know that. leave sutle hint about thier situation but keep it consice, and make sure they are quiet as the creature is close, and they are in danger. only anwser questions, and try to keep it vauge."},
-    {"role": "user", "content": question}
-  ]
-)
-
-response = (completion.choices[0].message.content)
-print(response)
-
-question = str(input(""))
-
-client = OpenAI(api_key=OPEN_AI_KEY)
-
-completion = client.chat.completions.create(
-  model="gpt-4o-mini",
-  store=True,
-  messages=[
-    {"role": "system", "content": "you are calling somone inside a mental hospital, there has been an attack and a mysterious creature is inside, the hospital is now abandonded. but the person your calling does not know that. leave sutle hint about thier situation but keep it consice, and make sure they are quiet as the creature is close, and they are in danger. only anwser questions, and try to keep it vauge."},
-    {"role": "user", "content": question}
-  ]
-)
-
-response = (completion.choices[0].message.content)
-print(response)
-
-question = str(input(""))
-
-client = OpenAI(api_key=OPEN_AI_KEY)
-
-completion = client.chat.completions.create(
-  model="gpt-4o-mini",
-  store=True,
-  messages=[
-    {"role": "system", "content": "you are calling somone inside a mental hospital, there has been an attack and a mysterious creature is inside, the hospital is now abandonded. but the person your calling does not know that. leave sutle hint about thier situation but keep it consice, and make sure they are quiet as the creature is close, and they are in danger. only anwser questions, and try to keep it vauge. try to end offf the conversation as the phone is going to cut off right after. try to continue the convo"},
-    {"role": "user", "content": question}
-  ]
-)
-
-response = (completion.choices[0].message.content)
-print(response)
-
-print()
-print("The phone has died")
-
-print()
-print('Do you fortify your room (F) or go out and explore (L)')
+print('Do you fortify your room (f) or go out and explore (l)')
 print()
 
 choice = input("Whats your next move ").strip().lower()
 
-if choice=="f":
-  
-  print("You decide to barricade the doors and hide. After a while you hear a loud banging Down the hall")
+#this and while loop if you go down the wrong path
+while choice !="l":
+  if choice=="f":
+    print("You decide to barricade the doors and hide. After a while you hear a loud banging Down the hall")
 
-  choice2=input("Do you leave the room to check out the noise(C) or do you Hide in the corner(H)").strip().lower()
+    choice = input("Do you leave the room to check out the noise (c) or do you Hide in the corner(h)").strip().lower()
   
-  while choice2 =="h":
-    print ("The noise get louder then stops abruptly followed by the sounds of faint murmur")
-    #ADD GAME RIGHT HERE
+    while choice =="h":
+      print ("The noise get louder then stops abruptly followed by the sounds of faint murmur")
+      #ADD GAME RIGHT HERE
 
-if choice == "l" or choice2 or "c":
+    while choice =="c":
+      choice = "l"
+
+  else:
+    print('Do you fortify your room (f) or go out and explore (l)')
+    choice=input("Whats your next move ").strip().lower()
+
+#If you choose to leave the room
+if choice == "l":
   
   print("You step outside into a hallway filled with flickering lights and smashed glass")
 
+#Hallway choice
   print()
   Choice=input("Do you head left (l) or right (r)").strip().lower()
   print()
 
+"""
+
+#If you keep going down the wrong path this is the code for that
 if Choice=="l":
   print ("as you start walking, the banging gets far louder, you start to question what you might find")
   print()
@@ -154,8 +49,9 @@ if Choice=="l":
     print("You didn't think it possible but rthe banging gets louder")
     #Another game
 
+#This is if you go the right way or turn back
 if Choice== "r" or Choice2=="t":
  Choice=input("As you walk along the hallway you stop three rooms. a lab (l), a storage room (s) and a patient room (p). which room would you like to enter").strip().lower()
 
-
+"""
 
